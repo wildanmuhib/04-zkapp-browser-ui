00c0db4878f1b8c47064d915e6915e335a5d79fb
root <root@vmi1049765.contaboserver.net>
2022-12-04 13:34:46 +0100

    Deploy gh-pages

--- ui/out/_next/static/chunks/webpack-22458de4b2b73bef.js
+++ ui/out/_next/static/chunks/webpack-22458de4b2b73bef.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "dd62de049e4d76e6" + ".css";
+/******/ 			return "static/css/" + "78a27facac621c17" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
